f64a9393843487fc77fabfcaa2779f5f76bc7d4d
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-11-12 19:02:57 +0530

    root route using express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/test", {
   useNewUrlParser: true,
@@ -7,4 +8,16 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("connected to mongodb");
+});
+
+// express
+const express = require("express");
+const app = express();
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
+// server
+const server = app.listen(3000, function() {
+  console.log("server started on port 3000");
 });
