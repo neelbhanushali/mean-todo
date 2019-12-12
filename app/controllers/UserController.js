67a1d1dff1fa91b108fd177ebeebd3b6e1ec5e06
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-12-12 20:13:47 +0530

    showing tokens with user using aggregate

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,8 +1,30 @@
+const UserModel = reqlib("app/models/UserModel").UserModel;
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
+
 module.exports = {
   list(req, res) {
     res.json({ url: req.originalUrl });
   },
-  show(req, res) {
-    res.json({ url: req.originalUrl, params: req.params });
+  async show(req, res) {
+    const user = await UserModel.aggregate([
+      {
+        $match: { _id: ObjectId(req.params.userId) }
+      },
+      {
+        $lookup: {
+          from: "userTokens",
+          localField: "_id",
+          foreignField: "user",
+          as: "tokens"
+        }
+      }
+    ]);
+
+    if (!user.length) {
+      return res.status(404).send("not found");
+    }
+
+    res.json(user[0]);
   }
 };
