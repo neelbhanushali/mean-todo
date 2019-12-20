8597d72eb8281c7fcc01ba8361285122ec5379dd
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-12-20 11:28:29 +0530

    show api

--- routes/Api/V1/Users.js
+++ routes/Api/V1/Users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const UserController = reqlib("app/controllers/UserController");
 
 router.get("/", UserController.list);
-router.get("/profile", UserController.getProfile);
+router.post("/:userId", UserController.show);
 router.get("/:userId/token/:token", UserController.activate);
 
 module.exports = router;
