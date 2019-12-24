910f2a6ff5cbcc4ad046653b54b3cfa422c47a48
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-12-24 19:56:51 +0530

    router.get for profile

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,7 +3,7 @@ const AuthMiddleware = reqlib("app/middlewares/AuthMiddleware");
 const AuthController = reqlib("app/controllers/v1/AuthController");
 
 router.use("/auth", require("./auth"));
-router.use("/profile", AuthMiddleware, AuthController.profile);
+router.get("/profile", AuthMiddleware, AuthController.profile);
 router.use("/todo", AuthMiddleware, require("./todo"));
 
 module.exports = router;
